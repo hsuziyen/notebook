025be26298974ca1afe54d84c8025f957e2f2aed
hsuziyen <67786214+hsuziyen@users.noreply.github.com>
2020-10-20 11:39:46 +0800

    Update App.vue

--- todo-list-vuejs-master/src/App.vue
+++ todo-list-vuejs-master/src/App.vue
@@ -301,7 +301,7 @@ div.delAll {
   text-decoration: none;
 }
 
-#app {
+div#app {
    font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
