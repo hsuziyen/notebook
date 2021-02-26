98206cae099745ff0d9651c60d72f5ab694bbfc6
hsuziyen <67786214+hsuziyen@users.noreply.github.com>
2021-02-26 10:35:31 +0800

    Update App.vue

--- todo-list-vuejs-master/src/App.vue
+++ todo-list-vuejs-master/src/App.vue
@@ -83,18 +83,18 @@ export default {
   },
   components: {
     itemsFilter:function (item){
-      var items= [];
-        for(i=0;i<items.label;i++) {
-          this.items.filter(item => {
-          if(items.label%2==0(this.search)) {
-            return this.items.push({label: this.newItem})
-              }
-           })            
-             console.log({label: this.newItem})
-               }
-        }
-     }
-  }
+        var items= [{label:'101'},{label:'102'},{label:'103'},{label:'201'},{label:'202'},{label:'300'},{label:'aaa'},{label:'abc'},{label:'bbb'}];
+          for(i=0;i<items.label;i++) {
+            var filtered=this.items.filter(function(items,index,arr) {
+              if(items.label>'201'(this.search)) {
+                return items.label.push(filtered);
+                  }
+               })            
+                 console.log(filtered);
+                   }
+          }
+       }
+    }
 </script>
 
 <style>
