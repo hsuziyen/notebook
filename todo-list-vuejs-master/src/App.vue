f8fda418962b680df901570c04451483d4280a6e
hsuziyen <67786214+hsuziyen@users.noreply.github.com>
2021-03-12 17:48:21 +0800

    Update App.vue

--- todo-list-vuejs-master/src/App.vue
+++ todo-list-vuejs-master/src/App.vue
@@ -96,7 +96,7 @@ class item {
   components: {
     doFilter: function(prefix) {
       return this.itemArray = this.items.filter(item => item.label.startsWith(prefix));
-        console.log('itemArray.length = ' + this.itemArray.length).includes(this.search).toLowerCase();
+        console.log('itemArray.length = ' + this.itemArray.length).includes(this.search);
           if (this.itemArray.length === 0) {
             this.errorMsg = '找不到 ' + prefix + ' 開頭的資料';
               this.itemArray = this.items;
